def52ef1090f18ca4b102393d6662d85064dced7
P3dr0HDev <pedrohribeiro.adv@gmail.com>
2026-09-24 21:10:59 -0300

    UserInfoForm()

--- src/components/UserInforForm.jsx
+++ src/components/UserInforForm.jsx
@@ -3,10 +3,13 @@ import { useState } from "react"
 function UserInforForm() {
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [dadosEnviados, setDadosEnviados] = useState(null); 
     
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(nome, email);        
+        setDadosEnviados({ nome, email, telefone });
+              
     }
 
   return (
@@ -14,6 +17,17 @@ function UserInforForm() {
         <form onSubmit={handleSubmit}>
             <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
             <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+            <button type="submit">Enviar</button>
+
+            {dadosEnviados && (
+            <div>
+                <h3>Dados enviados:</h3>
+                <p>Nome: {dadosEnviados.nome}</p>
+                <p>Email: {dadosEnviados.email}</p>
+                <p>Telefone: {dadosEnviados.telefone}</p>
+            </div>
+        )}
         </form>
     </div>
   )
